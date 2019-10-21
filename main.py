6498edffd7ffa43e0b03106503df06ae90804ca9
angela <angela@angelas-MacBook-Air.local>
2019-10-20 19:57:42 -0700

    Fix main.py to work with Parser object.

--- main.py
+++ main.py
@@ -2,21 +2,12 @@
 # Arithmetic Parser for Plexsys by Angela Ong
 # Be sure to use Python3+
 
-from parser import get_sum, evaluate_tree
+from parser import Parser
 
 while True:
-    # Get expression
     raw_expression = input("Enter expression to be evaluated: ")
-    if raw_expression == "exit": break
-
-    # Split expression by spaces
-    expression = raw_expression.split()
-    expression.append("end")
-
-    # build the tree
-    built_tree = get_sum(expression)
-
-    # evaluate the tree
-    result = evaluate_tree(built_tree)
-
-    print(result)
+    if raw_expression == "exit":
+        break
+    parser = Parser(raw_expression)
+    result = parser.parse()
+    print(result.evaluate())
